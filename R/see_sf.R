1fa0203063d0cd2381482edd6490a3252863ff0f
Dexter Locke <dexter.locke@gmail.com>
2024-08-14 20:28:11 -0400

    added documentation, clarified and removed dependencies

--- R/see_sf.R
+++ R/see_sf.R
@@ -1,13 +1,28 @@
 # # see which objects are of type sf
 # # thanks Kelly Hondula
 # # https://scholar.google.com/citations?user=Xdhvu9QAAAAJ&hl=en
-see_sf <- function(){
-  # what's in memory that are sf - spatial features?
-  purrr::keep(eapply(.GlobalEnv, class),              # gets the objects in the global environment
-              ~ any(stringr::str_detect(., "sf"))) |> # selects elements with sf in them
-    names() |>
-    as.character()       # my simple features, as a character vector
-}
+#' get the sf object in memory as a character string
+#'
+#' @import sf
+#' @import sfdep
+#' @return a character vector
+#' @export
+#'
+#' @examples
+#' see_sf()
+#' require(sfdep)
+#' test_sf_object <- guerry # a built in dataset in the sfdep package
+#' see_sf()
+see_sf <-
+  function(){
+    # what's in memory that are sf - simple features?
+    purrr::keep(eapply(.GlobalEnv, base::class),        # gets the objects in the global environment
+                ~ any(stringr::str_detect(., "sf"))) |> # selects elements with sf in them
+      names() |>
+      as.character()       # my simple features, as a character vector
+    }
+
+see_sf()
 
 # see_sf() -> sf_in_memory
 
