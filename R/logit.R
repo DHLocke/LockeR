3204ad06460762d80155da758c9f6bbb73c404fe
Dexter Locke <dexter.locke@gmail.com>
2024-11-09 18:03:02 -0500

    adding Readme documentation

--- R/logit.R
+++ R/logit.R
@@ -12,12 +12,17 @@
 #'                 , log_OR = log(OR)
 #'                 )
 #' look_up_probs |> dplyr::filter(OR == 1)
-inv_logit <- function(x){
-  # Calculate the inverse logit of a number
-  # copied directly from boot
+logit <- function(p){
+  qlogis(p)
+  # Calculate the logit of a proportion in the range [0,1]
+  # copied from boot
   # https://rdrr.io/cran/boot/src/R/bootfuns.q
-  out <- exp(x)/(1+exp(x))
-  out[x==-Inf] <- 0
-  out[x==Inf] <- 1
+  out <- p
+  inds <- seq_along(p)[!is.na(p)]
+    if (any((p[inds] < 0) | (p[inds] > 1)))
+      stop("invalid proportions input")
+  out[inds] <- log(p[inds]/(1-p[inds]))
+  out[inds][p[inds] == 0] <- -Inf
+  out[inds][p[inds] == 1] <- Inf
   out
   }
--- R/phone_it_in.R
+++ R/phone_it_in.R
@@ -15,16 +15,5 @@ phone_it_in <- function(phone, invalid = NA)
 }
 
 
-phone_it_in_parens <- function(phone, invalid = NA)
-{
-  phone <- gsub("[[:punct:]]", "", phone)          # remove punctuation
-  phone <- trimws(phone)                           # remove whitespace
-  phone[!nchar(phone) %in% c(7, 10)] <- invalid    # keep only 7 or 10 digit numbers
-  phone[nchar(phone) %in% 7] <- gsub("(^\\d{3})(\\d{4}$)",
-                                     "(\\1) \\2",
-                                     phone[nchar(phone) %in% 7])
-  phone[nchar(phone) %in% 10] <- gsub("(^\\d{3})(\\d{3})(\\d{4}$)",
-                                      "(\\1) \\2-\\3",  # where the parentheses are added
-                                      phone[nchar(phone) %in% 10])
-  phone
-}
+phone <- c("(123)-456-7890", "1234567890", "456890", "456-7890")
+phone_it_in(phone)
